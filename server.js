b48ded162fbbe11299f292981cd9487b58966513
Rohitsuresh1 <99383088+Rohitsuresh1@users.noreply.github.com>
2022-06-23 12:18:46 -0400

    added fetch routes and tables for all get routes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-// const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const db = require('./db/connections');
 const apiRoutes = require('./routes/apiRoutes');
+const fetch = require('node-fetch');
+const { response } = require('express');
+const { json } = require('body-parser');
+const { async } = require('rxjs');
 
 
 
@@ -30,31 +34,123 @@ db.connect(err => {
   });
 });
 
-// inquirer.prompt(
-//   {
-//       type: 'list',
-//       name: 'choise',
-//       message: 'Do you want to ',
-//       choices: ['View all roles','View all employees','Add a department','Add a role','Add an employee','Update an employee role']
-//   });
-// )
-// .then(({choice})=>{
-//   if(choice==='View all roles'){
-      
-//   }
-//   else if(choice==='View all employees'){
-      
-//   }
-//   else if(choice==='Add a department'){
-      
-//   }
-//   else if(choice==='Add a role'){
-      
-//   }
-//   else if(choice==='Add an employee'){
-      
-//   }
-//   else if(choice==='Update an employee role'){
-      
-//   }
-// });
+inquirer.prompt(
+  {
+      type: 'list',
+      name: 'choise',
+      message: 'Do you want to ',
+      choices: ['View all roles','View all employees','View all departments','View all employees by department','View all employees by manager','Add a department','Add a role','Add an employee','View total allocated department budjet','Update an employee role','Update an employee manager','Delete an employee','Delete a department','Delete a role','Exit']
+  })
+.then(({ choise }) => {
+  if(choise==='View all roles'){
+    allRoles();
+  }
+  else if(choise==='View all employees'){
+    allEmployees();
+  }
+  else if(choise==='View all departments'){
+    allDepartments();
+  }
+  else if(choise==='View all employees by department'){
+    viewByDepartment();
+  }
+  else if(choise==='View all employees by manager'){
+    viewByManager();
+  }
+  else if(choise==='View total allocated department budjet'){
+    console.log('budjet');
+  }
+  else if(choise==='Update an employee manager'){
+    console.log('update manager');
+  }
+  else if(choise==='Delete an employee'){
+    console.log('delete empl');
+  }
+  else if(choise==='Delete a department'){
+    console.log('delete dept');
+  }
+  else if(choise==='Delete a role'){
+    console.log("delete role");
+  }
+  else if(choise==='Add a department'){
+    console.log('add dept');
+  }
+  else if(choise==='Add a role'){
+    console.log('add role');
+  }
+  else if(choise==='Add an employee'){
+    console.log('add emplo');
+  }
+  else if(choise==='Update an employee role'){
+    console.log('update employee role');
+  }
+  else {
+  
+    // exit
+  }
+});
+
+async function allRoles() {
+  await fetch('http://localhost:3001/api/roles', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(data => data.text())
+    .then(text => {
+    let roles = JSON.parse(text);
+    console.table(roles.data);
+  })
+};
+
+async function allEmployees() {
+  await fetch('http://localhost:3001/api/employee', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(data => data.text())
+    .then(text => {
+    let rows = JSON.parse(text);
+    console.table(rows.data);
+  })
+};
+
+async function allDepartments() {
+  await fetch('http://localhost:3001/api/department', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(data => data.text())
+    .then(text => {
+    let rows = JSON.parse(text);
+    console.table(rows.data);
+  })
+};
+
+async function viewByDepartment() {
+  await fetch('http://localhost:3001/api/departmentsort', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(data => data.text())
+    .then(text => {
+    let rows = JSON.parse(text);
+    console.table(rows.data);
+  })
+};
+
+async function viewByManager() {
+  await fetch('http://localhost:3001/api/managersort', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(data => data.text())
+    .then(text => {
+    let rows = JSON.parse(text);
+    console.table(rows.data);
+  })
+};
